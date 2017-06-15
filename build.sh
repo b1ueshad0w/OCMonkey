43d777830d92fc7808330256e2b646e98ea6503c
gogleyin <gogleyin@tencent.com>
2017-06-15 17:38:05 +0800

    BugFix: Fix a bug in build.sh.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-xcodebuild build-for-testing OCMonkey.xcodeproj -scheme MonkeyRunner -derivedDataPath /tmp/derivedDataPath3 -sdk iphonesimulator
+xcodebuild build-for-testing -project OCMonkey.xcodeproj -scheme MonkeyRunner -derivedDataPath /tmp/derivedDataPath3 -sdk iphonesimulator
