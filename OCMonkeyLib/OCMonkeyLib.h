617958275fbbda526ffd340d2a56521417548db1
gogleyin <gogleyin@tencent.com>
2017-06-19 19:50:18 +0800

    BugFix: Fix issues when using OCMonkeyLib as a third-party framework in other projects: could not build module OCMonkeyLib.

--- OCMonkeyLib/OCMonkeyLib.h
+++ OCMonkeyLib/OCMonkeyLib.h
@@ -9,5 +9,45 @@
 #ifndef OCMonkeyLib_h
 #define OCMonkeyLib_h
 
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCMonkeyLib_.
+FOUNDATION_EXPORT double OCMonkeyLib_VersionNumber;
+
+//! Project version string for OCMonkeyLib_.
+FOUNDATION_EXPORT const unsigned char OCMonkeyLib_VersionString[];
+
+#import <OCMonkeyLib/AgentForHost.h>
+#import <OCMonkeyLib/ClassPath.h>
+#import <OCMonkeyLib/ClassPathItem.h>
+#import <OCMonkeyLib/ElementInfo.h>
+#import <OCMonkeyLib/ElementTree.h>
+#import <OCMonkeyLib/ElementTypeTransformer.h>
+#import <OCMonkeyLib/GGExceptionHandler.h>
+#import <OCMonkeyLib/GGLogger.h>
+#import <OCMonkeyLib/GGTestCase.h>
+#import <OCMonkeyLib/Keyboard.h>
+#import <OCMonkeyLib/Macros.h>
+#import <OCMonkeyLib/MathUtils.h>
+#import <OCMonkeyLib/Monkey+XCUITest.h>
+#import <OCMonkeyLib/Monkey+XCUITestPrivate.h>
+#import <OCMonkeyLib/Monkey.h>
+#import <OCMonkeyLib/MonkeyAlgorithm.h>
+#import <OCMonkeyLib/MonkeyConfiguration.h>
+#import <OCMonkeyLib/NSMutableArray+Queue.h>
+#import <OCMonkeyLib/NSMutableArray+Reverse.h>
+#import <OCMonkeyLib/NSMutableArray+Stack.h>
+#import <OCMonkeyLib/NSString-XCTAdditions.h>
+#import <OCMonkeyLib/NSValue-XCTestAdditions.h>
+#import <OCMonkeyLib/Random.h>
+#import <OCMonkeyLib/RandomAction.h>
+#import <OCMonkeyLib/RegularAction.h>
+#import <OCMonkeyLib/RunLoopSpinner.h>
+#import <OCMonkeyLib/SmartMonkey+UIChangeDelegate.h>
+#import <OCMonkeyLib/SmartMonkey.h>
+#import <OCMonkeyLib/Tree.h>
+#import <OCMonkeyLib/UIViewTree.h>
+#import <OCMonkeyLib/WeightedMonkey.h>
+
 
 #endif /* OCMonkeyLib_h */
